744db763afa62db078cc519a8a0b02f64905b7a7
Jackson Hill <43260963+jacksonhill-77@users.noreply.github.com>
2024-01-05 16:04:11 +1100

    Create App.jsx with HomePage import

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react';
+import HomePage from './HomePage'; // Import HomePage component
+
+function App() {
+  return (
+    <div>
+      <HomePage />  {/* Using HomePage component */}
+    </div>
+  );
+}
+
+export default App;
